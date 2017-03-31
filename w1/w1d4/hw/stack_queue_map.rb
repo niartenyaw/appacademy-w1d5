bdb394dbaba9da4bee8936b6a163419f8f7938d4
Aaron Wayne <amwayne90@gmail.com>
2017-03-30 21:04:04 -0700

    implement Map class with arrays

--- w1/w1d4/hw/stack_queue_map.rb
+++ w1/w1d4/hw/stack_queue_map.rb
@@ -56,3 +56,64 @@ p a.show
 4.times do
   puts a.dequeue
 end
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    idx = nil
+    @map.each_index { |i| idx = i if @map[i][0] == key}
+    if idx
+      @map[idx] = [key, value]
+    else
+      @map << [key, value]
+    end
+  end
+
+  def lookup(key)
+    @map.each do |pair|
+      return pair[1] if pair[0] == key
+    end
+    nil
+  end
+
+  def remove(key)
+    @map.each_index do |i|
+      if @map[i][0] == key
+        @map.delete_at(i)
+        break
+      end
+    end
+  end
+
+  def show
+    @map.each do |pair|
+      print "#{pair[0]} => #{pair[1]}\n"
+    end
+  end
+end
+
+
+a = Map.new
+10.times do |i|
+  a.assign(i.to_s, i)
+end
+
+a.show
+puts
+
+4.times do |i|
+  a.assign(i.to_s, i.to_s * 2)
+end
+
+a.show
+puts
+
+4.times do |i|
+  a.remove(i.to_s)
+end
+
+a.show
